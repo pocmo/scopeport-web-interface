c83dbef37301f7727e8249da7a6ba2c797868a0e
Lennart <lennart@scopeport.org>
2008-11-21 23:55:17 +0100

    Fixed bug: Form help highliter and notification group disabler now working together

--- public/javascripts/scopeport.js
+++ public/javascripts/scopeport.js
@@ -15,91 +15,8 @@ window.onload = function(){
   }
 };
  
-window.onload = function(){
-	// Fetch every input field.  
-  var inputs = document.getElementsByTagName("input");
-
-	// Go through every input field.  
-  for(i=0,l=inputs.length;i<l;i++){
-
-		// Get the current field.  
-    field = inputs[i];
-		
-		// Skipt if this field is not needed.  
-		if(field.name == "authenticity_token" || field.name == "commit")
-			continue;
-		
-		field.onfocus = function(){
-			// Position of start to substring.  
-			start = this.name.indexOf("[")+1;
-
-			// Get the name out of the field. e.g. "service[name]" gets "name"
-			fieldname = this.name.substr(start, this.name.length-start-1);
-
-			// Get the element we want to change the class of.
-    	helptext = document.getElementById("formhelp-" + fieldname);
-			if(helptext != null)
-				helptext.className = "formhelp-highlighted";
-    }
-
-		field.onblur = function(){
-			// Position of start to substring.  
-			start = this.name.indexOf("[")+1;
-
-			// Get the name out of the field. e.g. "service[name]" gets "name"
-			fieldname = this.name.substr(start, this.name.length-start-1);
-
-			// Get the element we want to change the class of.
-    	helptext = document.getElementById("formhelp-" + fieldname);
-			if(helptext != null)
-				helptext.className = "formhelp-normal";
-    }
-  }
-
-	// Fetch every select field.  
-  var selects = document.getElementsByTagName("select");
-	
-	// Go through every form field.  
-  for(i=0,l=selects.length;i<l;i++){
-
-		// Get the current field.  
-    field = selects[i];
-		
-		// Skipt if this field is not needed.  
-		if(field.name == "authenticity_token" || field.name == "commit")
-			continue;
-		
-		field.onfocus = function(){
-			// Position of start to substring.  
-			start = this.name.indexOf("[")+1;
-
-			// Get the name out of the field. e.g. "service[name]" gets "name"
-			fieldname = this.name.substr(start, this.name.length-start-1);
-
-			// Get the element we want to change the class of.
-    	helptext = document.getElementById("formhelp-" + fieldname);
-			if(helptext != null)
-				helptext.className = "formhelp-highlighted";
-    }
-    
-
-		field.onblur = function(){
-			// Position of start to substring.  
-			start = this.name.indexOf("[")+1;
-
-			// Get the name out of the field. e.g. "service[name]" gets "name"
-			fieldname = this.name.substr(start, this.name.length-start-1);
-
-			// Get the element we want to change the class of.
-    	helptext = document.getElementById("formhelp-" + fieldname);
-			if(helptext != null);
-				helptext.className = "formhelp-normal";
-		}
-	}
-};
-
 function disableOtherReceiverFields(thisfield){
-  var fields = new Array(  document.getElementById("newGroup_mail"),
+  var fields = new Array( document.getElementById("newGroup_mail"),
                           document.getElementById("newGroup_jid"),
                           document.getElementById("newGroup_numberc"));
   thisfieldName = "newGroup[" + thisfield + "]";
@@ -111,7 +28,7 @@ function disableOtherReceiverFields(thisfield){
     }
   }
 }
- 
+
 function clearFieldWithText(field, standardtext){
   if(field.value == standardtext)
     field.value = "";
