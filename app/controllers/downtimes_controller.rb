d82ea009e64dad44469ce0ecb8db56592abfb339
Ernesto Rocha <ernestorrocha@gmail.com>
2008-12-02 19:46:00 -0300

    Created Downtime model/controller and some views. And a bug fix.

---
+++ app/controllers/downtimes_controller.rb
@@ -0,0 +1,28 @@
+# This file is part of ScopePort (Web Interface).
+#
+# Copyright 2007, 2008 Lennart Koopmann
+#
+# ScopePort (Web Interface) is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ScopePort (Web Interface) is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ScopePort (Web Interface).  If not, see <http://www.gnu.org/licenses/>.
+
+class DowntimesController < ApplicationController
+  
+  def index
+    @downtimes = Downtime.find(:all)
+  end
+  
+  def show
+    @service = Service.find_by_id params[:id]
+  end
+  
+end
