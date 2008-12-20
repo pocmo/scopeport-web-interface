fdbb697c95e927c680bd2655935d4e4a82b02a57
ernestorocha <ernestorrocha@gmail.com>
2008-12-20 10:30:08 -0300

    Changed the login view and filters

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -1,7 +1,7 @@
 # This controller handles the login/logout function of the site.  
 class SystemsController < ApplicationController
   
-  skip_before_filter :login_required
+  skip_before_filter :login_required, :except => :destroy
   
   # render new.rhtml
   def new
