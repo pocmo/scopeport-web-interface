003dada36db00dff9153c97f82fe3d3ffb401328
Lennart <lennart@scopeport.org>
2008-12-30 15:28:41 +0100

    Changed error message for failed logins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -30,14 +30,14 @@ class SessionsController < ApplicationController
 
   def destroy
     logout_killing_session!
-    flash[:notice] = "You have been logged out."
+    flash[:notice] = "You have been logged out"
     redirect_back_or_default('/')
   end
 
 protected
   # Track failed login attempts
   def note_failed_signin
-    flash[:error] = "Couldn't log you in as '#{params[:login]}'"
+    flash[:error] = "Login failed! Wrong username or password."
     logger.warn "Failed login for '#{params[:login]}' from #{request.remote_ip} at #{Time.now.utc}"
   end
 end
