c8b4fac766e3c3679c4b5875fddf5727b66dd51b
ernestorocha <ernestorrocha@gmail.com>
2008-12-30 20:08:17 -0300

    Added a checkbox in signup view

--- db/migrate/20081218212552_create_users.rb
+++ db/migrate/20081218212552_create_users.rb
@@ -10,7 +10,7 @@ class CreateUsers < ActiveRecord::Migration
       t.column :updated_at,                :datetime
       t.column :remember_token,            :string, :limit => 40
       t.column :remember_token_expires_at, :datetime
-			t.column :admin											 :boolean
+			t.column :admin,										 :boolean
 
     end
     add_index :users, :login, :unique => true
