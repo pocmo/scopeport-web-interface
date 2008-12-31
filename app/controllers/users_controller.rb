8471b9aad7a4eab1bc6374438d0008e83d5107da
Lennart <lennart@scopeport.org>
2008-12-31 18:34:37 +0100

    Removed automatic login as newly created user after creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,6 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     success = @user && @user.save
     if success && @user.errors.empty?
-            # Protects against session fixation attacks, causes request forgery
-      # protection if visitor resubmits an earlier form using back
-      # button. Uncomment if you understand the tradeoffs.
-      # reset session
-      self.current_user = @user # !! now logged in
       redirect_back_or_default('/')
       flash[:notice] = "User created."
     else
