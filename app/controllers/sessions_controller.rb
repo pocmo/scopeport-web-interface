f7306e8bfa87864949181d70783738da2f5cf19c
ernestorocha <ernestorrocha@gmail.com>
2009-02-09 19:23:21 -0300

    "Last login" attribute is updating properly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,8 @@ class SessionsController < ApplicationController
     logout_keeping_session!
     user = User.authenticate(params[:login], params[:password])
     if user
+      #Updates the last login collumn 
+    	user.update_attribute("last_login", Time.now)
       # Protects against session fixation attacks, causes request forgery
       # protection if user resubmits an earlier form using back
       # button. Uncomment if you understand the tradeoffs.
