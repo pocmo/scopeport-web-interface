604786fc0254cd51e8796037fff24dcf5440781e
ernestorocha <ernestorrocha@gmail.com>
2009-02-19 14:59:51 -0300

    Quick fix to services with nil servicegroup_id

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -4,7 +4,9 @@ class ServicesController < ApplicationController
 
 	def index
 		@service_groups = Servicegroup.find :all
-    @services_without_group = Service.find_all_by_servicegroup_id nil || 0
+    @services_without_group = Service.find_all_by_servicegroup_id nil
+    @services_without_group.concat Service.find_all_by_servicegroup_id 0
+    
 
     # Count the services
 		@service_count = 0
@@ -23,6 +25,7 @@ class ServicesController < ApplicationController
 	end
 
 	def	create
+		p params[:service]
 		@service = Service.new(params[:service])
 		@notigroups = Notificationgroupdetail.find(:all).collect {|p| [p.name, p.id] }
 		@notigroups << ["None", "0"]	
