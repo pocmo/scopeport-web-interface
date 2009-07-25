7bdeda8e2b97c7cc68cc2a4ba9dded9d8db3b26e
lennart <lennart@amelia.(none)>
2009-07-26 00:00:34 +0200

    Deleted fail-migration that caused an migration error.

--- db/migrate/20090628230754_add_conversation_id_to_conversations.rb
+++
@@ -1,9 +0,0 @@
-class AddConversationIdToConversations < ActiveRecord::Migration
-  def self.up
-    add_column :conversations, :conversation_id, :integer
-  end
-
-  def self.down
-    remove_column :conversations, :conversation_id
-  end
-end
