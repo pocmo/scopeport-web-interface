11e8ce9ef77edead902832753299c8508e43dc41
Sebastian Kaspari <s.kaspari@googlemail.com>
2009-12-08 21:40:31 +0100

    show graph title on hover (preview)

--- app/helpers/hosts_helper.rb
+++ app/helpers/hosts_helper.rb
@@ -1,7 +1,7 @@
 module HostsHelper
 
-  def showGraphDummy name, host_id, graph_days, token
-    image_tag "blurred_graphs/#{name}.png", :id => "graph-#{name}-blurred", :style => "cursor: pointer;", :onclick => "showGraph('#{name}', '#{host_id}', '#{graph_days}', '#{token}')"
+  def showGraphDummy name, host_id, graph_days, token, title = ""
+    image_tag "blurred_graphs/#{name}.png", :id => "graph-#{name}-blurred", :style => "cursor: pointer;", :onclick => "showGraph('#{name}', '#{host_id}', '#{graph_days}', '#{token}')", :title => title
   end
 
 end
